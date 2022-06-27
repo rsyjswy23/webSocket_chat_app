fe26f7a258411d02eff885524970db6d2303e793
rsyjswy23 <summerha0211@gmail.com>
2022-06-27 15:18:34 -0700

    server setup & enable channel layer with Redis
    enable channel layer to let multiple comsumer instances to communicate with each other

---
+++ backend/chat/urls.py
@@ -0,0 +1,10 @@
+# chat/urls.py
+from django.urls import path
+from .views import index, room
+
+app_name = 'chat'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('<str:room_name>/', room, name='room'),
+]
